8bc03411bd02b2722e81d710dc8eae614f51ec58
Manisha Meher <mehermanisha2005@gmail.com>
2026-04-16 16:41:21 +0530

    Added templates

---
+++ src/main/java/com/cwm/studentmanagement/StudentmanagementApplication.java
@@ -0,0 +1,17 @@
+package com.cwm.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StudentmanagementApplication {
+
+	public static void main(String[] args) {
+		
+		// 🔥 ADD THIS LINE
+		
+
+		SpringApplication.run(StudentmanagementApplication.class, args);
+	}
+}
